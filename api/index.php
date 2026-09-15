751d2866a42bb9bd5387da71b03d3276230c675c
PIXWAR <pixwar@users.noreply.github.com>
2026-09-15 09:14:38 +0700

    debug: test HandleRequest

--- api/index.php
+++ api/index.php
@@ -4,3 +4,13 @@ echo "Autoload OK<br>";
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 echo "Bootstrap OK<br>";
+
+try {
+    $app->handleRequest(Illuminate\Http\Request::capture());
+} catch (\Throwable $e) {
+    echo "<pre>";
+    echo "Error: " . $e->getMessage() . "\n";
+    echo "File: " . $e->getFile() . "\n";
+    echo "Line: " . $e->getLine() . "\n";
+    echo "</pre>";
+}
